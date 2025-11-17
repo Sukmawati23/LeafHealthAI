5c2b1b808535df30e357cdd52c6493e718a44a78
Iis Sukmawati <ticasyncle@gmail.com>
2025-11-17 19:10:21 +0700

    Test

---
+++ src/gui/main_window.py
@@ -0,0 +1,6 @@
+import tkinter as tk  
+root = tk.Tk()  
+root.title("LeafHealthAI")  
+tk.Label(root, text="Selamat datang di LeafHealthAI", font=("Arial", 16)).pack(pady=50)  
+root.geometry("400x200")  
+root.mainloop()  
